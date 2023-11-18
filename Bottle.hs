4404996c0fd6c916171bd85f2cbc1dcdee043adb
Marc Ziegert <comonad@gmail.com>
2023-11-18 16:26:21 +0100

    Bottle

--- Bottle.hs
+++ Bottle.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE NamedFieldPuns #-}
 
 
 module Bottle where
@@ -31,46 +32,55 @@ instance Show Color where
 
 
 
-newtype Bottle = Bottle_ [Color] deriving (Eq,Ord)
 makeBottle :: [Color] -> Bottle
-makeBottle = Bottle_
-
-
-
-
-
+makeBottle cs = Bottle_ [(List.length g,c)|g@(c:_)<-List.group cs] (List.length cs)
 
 type Liquid = (Int,Color)
+data Bottle = Bottle_
+    { bottleLiquids :: [Liquid]
+    , bottleFillLevel :: !Int
+    } deriving (Eq,Ord)
+
+
+
 bottleTopLiquid :: Bottle -> Maybe Liquid
-bottleTopLiquid bottle@(Bottle_ bottle_) = do
-    color <- bottleTopColor bottle
-    return (List.length $ List.takeWhile(==color) bottle_,color)
+bottleTopLiquid bottle = listToMaybe $ bottleLiquids bottle
 bottleTopColor :: Bottle -> Maybe Color
-bottleTopColor (Bottle_ bottle) = do
-    (color:_) <- Just bottle
-    return color
+bottleTopColor bottle = snd <$> bottleTopLiquid bottle
 bottleUniColor :: Bottle -> Maybe Color
-bottleUniColor (Bottle_ bottle) = do
-    (color:_) <- Just bottle
-    guard $ List.all (==color) bottle
-    return color
-bottleFillLevel :: Bottle -> Int
-bottleFillLevel (Bottle_ bottle) = List.length bottle
+bottleUniColor bottle = do
+    case bottleLiquids bottle of
+        [(_,c)] -> Just c
+        _ -> Nothing
+--bottleFillLevel :: Bottle -> Int
+--bottleFillLevel (Bottle_ bottle) = List.length bottle
 bottleFreeSpace :: Bottle -> Int
 bottleFreeSpace bottle = bottleMaxHeight - bottleFillLevel bottle
 bottleIsEmpty :: Bottle -> Bool
-bottleIsEmpty (Bottle_ bottle) = List.null bottle
+bottleIsEmpty bottle = bottleFillLevel bottle == 0
 bottleIsFull :: Bottle -> Bool
 bottleIsFull bottle = bottleFillLevel bottle == bottleMaxHeight
 bottleIsUnicolor :: Color -> Bottle -> Bool
-bottleIsUnicolor color (Bottle_ []) = False
-bottleIsUnicolor color (Bottle_ bottle) = List.all (==color) bottle
+bottleIsUnicolor color bottle = bottleUniColor bottle == Just color
 bottleIsComplete :: Bottle -> Bool
-bottleIsComplete bottle = (fst<$>bottleTopLiquid bottle) == Just bottleMaxHeight
+bottleIsComplete bottle = (fst<$>bottleLiquids bottle) == [bottleMaxHeight]
 bottleDropN :: Int -> Bottle -> Bottle
-bottleDropN n (Bottle_ bottle) = Bottle_ $ List.drop n bottle
+bottleDropN 0 bottle = error "bottleDropN 0"
+bottleDropN n (Bottle_{bottleLiquids=((k,c):ls),bottleFillLevel})|k<n =
+    bottleDropN (n-k) Bottle_{bottleLiquids=ls,bottleFillLevel=bottleFillLevel-k}
+bottleDropN n (Bottle_{bottleLiquids=((k,c):ls),bottleFillLevel})|k==n =
+    Bottle_{bottleLiquids=ls,bottleFillLevel=bottleFillLevel-n}
+bottleDropN n (Bottle_{bottleLiquids=((k,c):ls),bottleFillLevel})|k>n =
+    Bottle_{bottleLiquids=(k-n,c):ls,bottleFillLevel=bottleFillLevel-n}
 bottleFillN :: Int -> Color -> Bottle -> Bottle
-bottleFillN n color (Bottle_ bottle) = Bottle_ $ List.replicate n color <> bottle
+bottleFillN 0 color bottle = error "bottleFillN 0"
+bottleFillN n color (Bottle_{bottleLiquids=((k,c):ls),bottleFillLevel}) | c==color =
+    Bottle_{bottleLiquids=(n+k,color):ls,bottleFillLevel=bottleFillLevel+n}
+bottleFillN n color (Bottle_{bottleLiquids,bottleFillLevel}) =
+    Bottle_{bottleLiquids=(n,color):bottleLiquids,bottleFillLevel=bottleFillLevel+n}
+
+
+
 bottleTransferFromTo :: (Bottle,Bottle) -> Maybe (Bottle,Bottle)
 bottleTransferFromTo (fromBottle,toBottle) = do
     (n,color)<-bottleTopLiquid fromBottle
