7710f3cf629bf204c5b3a42579ce212f3210f679
Marc Ziegert <comonad@gmail.com>
2023-11-18 15:59:38 +0100

    refactoring Bottle

--- ColorSort.hs
+++ ColorSort.hs
@@ -21,21 +21,8 @@ import Data.Foldable as Foldable
 
 import FreeMonoid
 
+import Bottle
 
-
-bottleMaxHeight = 4 :: Int
-
-
-newtype Color = Color_ Int deriving (Eq,Ord)
-[ash,blue,earth,green,lila,mint,orange,pink,red,sky,weed,yellow] = Color_ <$> [1..12]
-instance Show Color where
-    show (Color_ c) = List.words "ash blue earth green lila mint orange pink red sky weed yellow" !! (c-1)
-
-
-
-newtype Bottle = Bottle_ [Color] deriving (Eq,Ord)
-makeBottle :: [Color] -> Bottle
-makeBottle = Bottle_
 
 type Level = [Bottle]
 level141 :: Level
@@ -342,46 +329,6 @@ apply' level (from,to,color) = (apply ft level,ft')
                     else to'
       ft = (from',to'',color)
       ft' = (from',to'',color,n)
-
-
-type Liquid = (Int,Color)
-bottleTopLiquid :: Bottle -> Maybe Liquid
-bottleTopLiquid bottle@(Bottle_ bottle_) = do
-    color <- bottleTopColor bottle
-    return (List.length $ List.takeWhile(==color) bottle_,color)
-bottleTopColor :: Bottle -> Maybe Color
-bottleTopColor (Bottle_ bottle) = do
-    (color:_) <- Just bottle
-    return color
-bottleUniColor :: Bottle -> Maybe Color
-bottleUniColor (Bottle_ bottle) = do
-    (color:_) <- Just bottle
-    guard $ List.all (==color) bottle
-    return color
-bottleFillLevel :: Bottle -> Int
-bottleFillLevel (Bottle_ bottle) = List.length bottle
-bottleFreeSpace :: Bottle -> Int
-bottleFreeSpace bottle = bottleMaxHeight - bottleFillLevel bottle
-bottleIsEmpty :: Bottle -> Bool
-bottleIsEmpty (Bottle_ bottle) = List.null bottle
-bottleIsFull :: Bottle -> Bool
-bottleIsFull bottle = bottleFillLevel bottle == bottleMaxHeight
-bottleIsUnicolor :: Color -> Bottle -> Bool
-bottleIsUnicolor color (Bottle_ []) = False
-bottleIsUnicolor color (Bottle_ bottle) = List.all (==color) bottle
-bottleIsComplete :: Bottle -> Bool
-bottleIsComplete bottle = (fst<$>bottleTopLiquid bottle) == Just bottleMaxHeight
-bottleDropN :: Int -> Bottle -> Bottle
-bottleDropN n (Bottle_ bottle) = Bottle_ $ List.drop n bottle
-bottleFillN :: Int -> Color -> Bottle -> Bottle
-bottleFillN n color (Bottle_ bottle) = Bottle_ $ List.replicate n color <> bottle
-bottleTransferFromTo :: (Bottle,Bottle) -> Maybe (Bottle,Bottle)
-bottleTransferFromTo (fromBottle,toBottle) = do
-    (n,color)<-bottleTopLiquid fromBottle
-    let k = bottleFreeSpace toBottle
-    let t = n `min` k
-    guard $ bottleIsEmpty toBottle || bottleTopColor toBottle == Just color
-    return (bottleDropN t fromBottle, bottleFillN t color toBottle)
 
 
 
